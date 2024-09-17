44dd342c5cf9d4786a22ea9a738952f8f4aa7573
eugek0 <palma21042005@gmail.com>
2024-09-17 21:36:17 +0300

    Добавлена очистка даннных профиля после выхода из аккаунта.

--- src/containers/Common/ProfileMenuPopoverContainer/index.tsx
+++ src/containers/Common/ProfileMenuPopoverContainer/index.tsx
@@ -1,9 +1,15 @@
 import ProfileMenuPopover from "@/components/Common/ProfileMenuPopover";
-import { useGetProfileQuery, useLogoutMutation } from "@/containers/Auth/api";
+import {
+  authApi,
+  useGetProfileQuery,
+  useLogoutMutation,
+} from "@/containers/Auth/api";
 import { useNavigate } from "@tanstack/react-router";
 import { FC, MouseEventHandler } from "react";
+import { useDispatch } from "react-redux";
 
 const ProfileMenuPopoverContainer: FC = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const { data } = useGetProfileQuery();
@@ -11,6 +17,7 @@ const ProfileMenuPopoverContainer: FC = () => {
 
   const handleLogout: MouseEventHandler<HTMLButtonElement> = async () => {
     await logout();
+    dispatch(authApi.util.resetApiState());
     navigate({ to: "/" });
   };
 
