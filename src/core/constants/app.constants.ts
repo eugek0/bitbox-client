58607782e95ddeff884c3aeb88feab54a6ad32ca
eugek0 <palma21042005@gmail.com>
2024-09-23 21:57:46 +0300

    Редизайн.

--- src/core/constants/app.constants.ts
+++ src/core/constants/app.constants.ts
@@ -1,1 +1,1 @@
-export const APP_NAME = "Cloudy";
+export const APP_NAME = "Stash";
---
+++ src/core/theme.ts
@@ -0,0 +1,15 @@
+import { ThemeConfig } from "antd";
+
+export const theme: ThemeConfig = {
+  token: {
+    colorPrimary: "#06D001",
+    colorPrimaryBg: "#04b800",
+    colorText: "#fff",
+    colorBgBase: "#010409",
+    colorBorder: "#fff",
+    colorLink: "#06D001",
+    colorTextPlaceholder: "gray",
+    colorIcon: "#fff",
+    colorBgElevated: "#010814",
+  },
+};
