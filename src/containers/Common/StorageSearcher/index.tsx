18908c2f11fbf5705e637310927dad58b0d61f40
eugek0 <jmaster21042005@mail.ru>
2025-02-21 22:30:26 +0300

    Рендер иконки хранилища при поиске.

--- src/containers/Common/StorageSearcher/index.tsx
+++ src/containers/Common/StorageSearcher/index.tsx
@@ -1,12 +1,26 @@
 import { FC } from "react";
-import { AutoComplete } from "antd";
+import { AutoComplete, Flex, Typography } from "antd";
 import { AutoCompleteProps } from "antd/lib";
 import { debounce } from "lodash";
 import { useLazySearchStoragesOptionsQuery } from "@/containers/Storages/api";
 import { StorageSearcherProps } from "./types";
+import { ProductFilled } from "@ant-design/icons";
 
 const StorageSearcher: FC<StorageSearcherProps> = (props) => {
-  const [search, { data: options }] = useLazySearchStoragesOptionsQuery();
+  const [search, { data: options }] = useLazySearchStoragesOptionsQuery({
+    selectFromResult: (response) => ({
+      ...response,
+      data: response.data?.map((option) => ({
+        ...option,
+        label: (
+          <Flex align="center" gap={10}>
+            <ProductFilled />
+            <Typography.Text>{option.value}</Typography.Text>
+          </Flex>
+        ),
+      })),
+    }),
+  });
 
   const handleSearch: AutoCompleteProps["onSearch"] = debounce((name) => {
     search({ name });
