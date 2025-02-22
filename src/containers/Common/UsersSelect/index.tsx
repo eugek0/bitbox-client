46a5bba48142dcf7613eac1ff1ebef645c189dc9
eugek0 <jmaster21042005@mail.ru>
2025-02-22 23:24:38 +0300

    Убран текущий пользователь из выпадающего списка пользователей.

--- src/containers/Common/UsersSelect/index.tsx
+++ src/containers/Common/UsersSelect/index.tsx
@@ -2,14 +2,18 @@ import { FC } from "react";
 import { Avatar, Flex, Select, Typography } from "antd";
 import { SelectProps } from "antd/lib";
 import { useGetUsersOptionsQuery } from "@/core/api";
+import { useAppSelector } from "@/store";
+import { profileIdSelector } from "@/containers/Auth/selectors";
 
 const UsersSelect: FC<SelectProps> = (props) => {
+  const id = useAppSelector(profileIdSelector);
+
   const { data: options, isFetching } = useGetUsersOptionsQuery();
 
   return (
     <Select
       {...props}
-      options={options}
+      options={options?.filter((option) => option.value !== id) ?? []}
       loading={isFetching}
       optionRender={(option) => (
         <Flex align="center" gap={10}>
