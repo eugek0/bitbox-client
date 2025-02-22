f3199ce268354da0da8dc96b8b41af4405737604
eugek0 <jmaster21042005@mail.ru>
2025-02-22 14:48:40 +0300

    Патч для 19 реакта

--- src/containers/Common/StorageSearcher/index.tsx
+++ src/containers/Common/StorageSearcher/index.tsx
@@ -1,26 +1,14 @@
 import { FC } from "react";
 import { AutoComplete, Flex, Typography } from "antd";
+import { ProductFilled } from "@ant-design/icons";
 import { AutoCompleteProps } from "antd/lib";
 import { debounce } from "lodash";
 import { useLazySearchStoragesOptionsQuery } from "@/containers/Storages/api";
 import { StorageSearcherProps } from "./types";
-import { ProductFilled } from "@ant-design/icons";
+import styles from "./styles.module.scss";
 
 const StorageSearcher: FC<StorageSearcherProps> = (props) => {
-  const [search, { data: options }] = useLazySearchStoragesOptionsQuery({
-    selectFromResult: (response) => ({
-      ...response,
-      data: response.data?.map((option) => ({
-        ...option,
-        label: (
-          <Flex align="center" gap={10}>
-            <ProductFilled />
-            <Typography.Text>{option.value}</Typography.Text>
-          </Flex>
-        ),
-      })),
-    }),
-  });
+  const [search, { data: options }] = useLazySearchStoragesOptionsQuery();
 
   const handleSearch: AutoCompleteProps["onSearch"] = debounce((name) => {
     search({ name });
@@ -30,6 +18,12 @@ const StorageSearcher: FC<StorageSearcherProps> = (props) => {
     <AutoComplete
       onSearch={handleSearch}
       options={options ?? []}
+      optionRender={(option) => (
+        <Flex align="center" gap={10}>
+          <ProductFilled className={styles["icon"]} />
+          <Typography.Text>{option.data.value}</Typography.Text>
+        </Flex>
+      )}
       allowClear
       {...props}
     />
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { store } from "./store";
 import { appTheme } from "./core/theme";
 import Root from "./containers/Root";
 import ruRu from "antd/locale/ru_RU";
+import "@ant-design/v5-patch-for-react-19";
 import "@/core/styles/normalize.css";
 import "@/core/styles/defaults.scss";
 
