5ec8413c9477c426e4e22aac4b1e40022badd651
eugek0 <jmaster21042005@mail.ru>
2025-03-22 14:14:21 +0300

    Добавлен функциональный поиск хранилищ

--- src/containers/Common/StorageSearcher/index.tsx
+++ src/containers/Common/StorageSearcher/index.tsx
@@ -6,17 +6,25 @@ import { debounce } from "lodash";
 import { useLazySearchStoragesOptionsQuery } from "@/containers/Storages/api";
 import { StorageSearcherProps } from "./types";
 import styles from "./styles.module.scss";
+import { useNavigate } from "@tanstack/react-router";
 
 const StorageSearcher: FC<StorageSearcherProps> = (props) => {
+  const navigate = useNavigate();
+
   const [search, { data: options }] = useLazySearchStoragesOptionsQuery();
 
   const handleSearch: AutoCompleteProps["onSearch"] = debounce((name) => {
     search({ name });
   }, 500);
 
+  const handleSelect: AutoCompleteProps["onSelect"] = (_: string, option) => {
+    navigate({ to: `/storage/${option._id}` });
+  };
+
   return (
     <AutoComplete
       onSearch={handleSearch}
+      onSelect={handleSelect}
       options={options ?? []}
       optionRender={(option) => (
         <Flex align="center" gap={10}>
