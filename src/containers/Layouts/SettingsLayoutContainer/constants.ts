5de14f5e647daf395dec6d5a3e7f7ff1dda7b0d4
eugek0 <jmaster21042005@mail.ru>
2025-05-03 01:22:13 +0300

    fix

--- src/containers/Layouts/SettingsLayoutContainer/constants.ts
+++ src/containers/Layouts/SettingsLayoutContainer/constants.ts
@@ -3,6 +3,6 @@ import { TRole } from "@/containers/Auth/types";
 export const SETTINGS_LAYOUT_PERMISSIONS: Record<string, TRole[]> = {
   "/settings/profile": ["owner", "administrator", "user"],
   "/settings/security": ["owner", "administrator", "user"],
-  "/settings/development": ["owner", "administrator"],
+  "/settings/development": ["owner", "administrator", "user"],
   "/settings/roles": ["owner", "administrator"],
 };
